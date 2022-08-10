afe27bb6e62103df04faf8c945c0916b056255e9
Sirineo-Grohmann <88437409+Sirineo-Grohmann@users.noreply.github.com>
2022-08-10 14:13:40 -0300

    Update informe.java

--- informe.java
+++ informe.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
+
 public class informe {
     public static void main (String[]args){
         Scanner entrada = new Scanner (System.in);
